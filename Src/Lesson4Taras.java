ef8dd2ea1256f42a704503a143c518e77a4ba594
Taras12345678 <Taras.kanbud@ukr.net>
2023-08-04 20:43:14 +0300

    third massege

--- Src/Lesson4Taras.java
+++ Src/Lesson4Taras.java
@@ -2,6 +2,7 @@ public class Lesson4Taras {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git from Taras");
+        System.out.println("Hello git from our group");
     }
 
 }
